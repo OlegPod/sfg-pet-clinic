db508f3ebbadc6476c3f43af41fb4e8fe3742950
OlegPod <kepacuh@gmail.com>
2019-03-11 22:41:04 +0200

    Create interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/com/olehpodolin/sfgpetclinic/services/VetService.java
@@ -0,0 +1,15 @@
+package com.olehpodolin.sfgpetclinic.services;
+
+
+import com.olehpodolin.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}

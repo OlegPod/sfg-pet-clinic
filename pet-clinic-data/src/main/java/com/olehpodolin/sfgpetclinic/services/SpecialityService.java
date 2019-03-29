05b14f500cb35bd48081e5a6a9bbf0781d96a5c6
OlegPod <kepacuh@gmail.com>
2019-03-29 05:32:01 +0200

    Created Specialty Map Service Impl.Closes #31.

---
+++ pet-clinic-data/src/main/java/com/olehpodolin/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.olehpodolin.sfgpetclinic.services;
+
+import com.olehpodolin.sfgpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}

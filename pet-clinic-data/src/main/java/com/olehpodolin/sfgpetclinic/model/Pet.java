322711133465813d4dcfb083ab79cc49630e6bce
OlegPod <kepacuh@gmail.com>
2019-04-15 02:25:10 +0300

    Updated Owner with JPA mappings.Closes #36.

--- pet-clinic-data/src/main/java/com/olehpodolin/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/olehpodolin/sfgpetclinic/model/Pet.java
@@ -1,12 +1,24 @@
 package com.olehpodolin.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends Person{
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate bithDate;
 
     public String getName() { return name; }

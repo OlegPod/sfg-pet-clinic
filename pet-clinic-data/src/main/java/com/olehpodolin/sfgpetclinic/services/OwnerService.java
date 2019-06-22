8c07c6b1e53cfce40e1c962e15f9337bd24c075a
OlegPod <kepacuh@gmail.com>
2019-06-22 21:21:27 +0300

    Implemented Find Owner feature. Closes #54.

--- pet-clinic-data/src/main/java/com/olehpodolin/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/olehpodolin/sfgpetclinic/services/OwnerService.java
@@ -2,9 +2,13 @@ package com.olehpodolin.sfgpetclinic.services;
 
 import com.olehpodolin.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 
 public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
--- pet-clinic-data/src/main/java/com/olehpodolin/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/olehpodolin/sfgpetclinic/services/map/OwnerMapService.java
@@ -8,6 +8,7 @@ import com.olehpodolin.sfgpetclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 @Service
 @Profile({"default", "map"})
@@ -74,5 +75,11 @@ public class OwnerMapService extends AbstactMapService<Owner, Long> implements O
                 .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+        // todo implement this
+        return null;
     }
 }
